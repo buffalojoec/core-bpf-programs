0548d09fa80076aca13b269482d2051f74baf781
Joe <jcaulfield135@gmail.com>
2023-12-01 11:05:27 -0600

    address lookup table: final draft

--- address-lookup-table/program/src/lib.rs
+++ address-lookup-table/program/src/lib.rs
@@ -8,3 +8,7 @@ pub mod error;
 pub mod processor;
 
 solana_program::declare_id!("AddressLookupTab1e1111111111111111111111111");
+
+// Export current sdk types for downstream users building with a different sdk
+// version
+pub use solana_program::address_lookup_table::{instruction, state};
